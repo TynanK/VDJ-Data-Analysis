a241c9cb2da4bc1907dd29ab971d45c71543439f
Tynan Kennedy <tynankennedy@gmail.com>
2020-08-01 15:50:06 -0700

    Improved modularity by functionalizing sorting

--- arrayManipulations.py
+++ arrayManipulations.py
@@ -98,11 +98,7 @@ def mergeLists(data1, key1, data2, key2):
         for b in range(len(data2[a])):
             data3, key3 = dataInsert(data3, key3, data2[a][b], key2[a])
     
-    key3_sorted = key3
-    key3_sorted.sort()
-    if key3_sorted != key3:
-        data3 = [x for _,x in sorted(zip(data3, key3))]
-        key3 = key3_sorted
+    data3, key3 = sort2D(data3, key3)
 
     return data3, key3
 
@@ -119,19 +115,38 @@ def mergeLists3D(data1, alpha_key_list1, beta_key_listoflists1, data2, alpha_key
             for c in range(len(data2[a][b])):
                 data3, alpha_key_list_3, beta_key_listoflists3 = dataInsert3D(data3, alpha_key_list3, beta_key_listoflists3, data2[a][b][c], alpha_key_list2[a], beta_key_listoflists2[a][b])
 
-    alpha_key_list3_sorted = alpha_key_list3
-    alpha_key_list3_sorted.sort()
-    if alpha_key_list3_sorted != alpha_key_list3:
-        data3 = [x for _,x in sorted(zip(data3, alpha_key_list3))]
-        beta_key_listoflists3 = [x for _,x in sorted(zip(beta_key_listoflists3, alpha_key_list3))]
-        alpha_key_list3 = alpha_key_list3_sorted
-    
-    len0 = len(alpha_key_list3)
-    for a in range(len0):
-        beta_key_list3_sorted = beta_key_list3[a]
-        beta_key_list3_sorted.sort()
-        if beta_key_list3_sorted != beta_key_list3[a]:
-            data3[a] = [x for _,x in sorted(zip(data3[a], beta_key_list3[a]))]
-            beta_key_list3[a] = beta_key_list3_sorted
+    data3, alpha_key_list3, beta_key_listoflists3 = sort3D(data3, alpha_key_list3, beta_key_listoflists3)
     
     return data3, alpha_key_list3, beta_key_listoflists3
+
+def sort2D(data, keys):
+    data1 = data
+    keys1 = keys
+    keys_sorted = keys1
+    keys_sorted.sort()
+    if keys_sorted != keys1:
+        data1 = [x for _,x in sorted(zip(data1, keys1))]
+        keys1 = keys_sorted
+    return data1, keys1
+
+def sort3D(data, alpha_key_list, beta_key_listoflists):
+    data1 = data
+    alpha_keys1 = alpha_key_list
+    beta_keys1 = beta_key_listoflists
+
+    alpha_keys_sorted = alpha_keys1
+    alpha_keys_sorted.sort()
+    if alpha_keys_sorted != alpha_keys1:
+        data1 = [x for _,x in sorted(zip(data1, alpha_keys1))]
+        beta_keys1 = [x for _,x in sorted(zip(beta_keys1, alpha_keys1))]
+        alpha_keys1 = alpha_keys_sorted
+    
+    len0 = len(alpha_keys1)
+    for a in range(len0):
+        beta_keys_sorted = beta_keys1[a]
+        beta_keys_sorted.sort()
+        if beta_keys_sorted != beta_keys1[a]:
+            data1[a] = [x for _,x in sorted(zip(data1[a], beta_keys1[a]))]
+            beta_keys1[a] = beta_keys_sorted
+    
+    return data1, alpha_keys1, beta_keys1
