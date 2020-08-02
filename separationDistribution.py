e3574431e8f2d75e34e21e1fa5dd942e9d7400c5
Tynan Kennedy <tynankennedy@gmail.com>
2020-08-02 03:29:45 -0700

    Updated to handle multiple files.

--- separationDistribution.py
+++ separationDistribution.py
@@ -4,6 +4,15 @@
 
 # This script will take as inputs a separations trajectory and a number of bins, and generate the PDF of separations
 
+# It two possible input syntaxes, based on an optional first input. If the first parameter is "batch", it's type 1. Otherwise it's type 2.
+
+# Type 1: python3 separationDistribution.py batch [dataType] [numCells] [binCount]
+# This will perform the analysis of trajectory files of the form [dataType]_[1-numCells].npy. ex: VDJ_1.npy, VDJ_2.npy, ...., VDJ_[numCells].npy
+# binCount is just the number of bins to use for the PDF
+
+# Type 2: python3 separationDistribution.py [binCount] [filename(s)]
+# This will perform the analysis on the specific files listed. binCount means the same as before
+
 # The goal is to quantify the degree of overlap between trajectories.
 
 import numpy as np
@@ -56,8 +65,19 @@ def normalize(bins, binEdges):
 
 if __name__ == "__main__":
     argc = len(sys.argv)
-    assert (argc == 3), "Improper inputs. Proper syntax: python3 separationDistribution.py [separations file] [binCount]"
-    filename = str(sys.argv[1])
-    binCount = int(sys.argv[2])
+    assert (argc >= 3), "Improper inputs. Please see separationDistribution.py for your input options."
+    if str(sys.argv[1]) == "batch":
+        dataType = str(sys.argv[2])
+        numCells = int(sys.argv[3])
+        binCount = int(sys.argv[4])
+        filenames = []
+        for a in range(1,numCells+1):
+            filenames.append(dataType + "_" + str(a) + ".npy")
+    else:
+        binCount = int(sys.argv[1])
+        filenames = []
+        for a in range(2,argc):
+            filenames.append(sys.argv[a])
 
-    x = singleTrajectoryAnalysis(filename, binCount)
+    for a in range(len(filenames)):
+        x = singleTrajectoryAnalysis(filenames[a], binCount)
