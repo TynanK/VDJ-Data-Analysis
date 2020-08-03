22e936fc64886d0bfb80ffb0d529e349a71ac7c7
Tynan Kennedy <tynankennedy@gmail.com>
2020-08-03 03:04:24 -0700

    Merged redundant scripts

--- plotSeparationDistribution.py
+++ plotSeparationDistribution.py
@@ -3,8 +3,9 @@
 # Date: August 1, 2020
 
 # Plots a given separation vs t onto the figure and axis passed to the function
+
 # Command line syntax: python3 plotSeparationDistribution.py {red/blue/green} [filenames] ...
-# Where the filenames are of the form PDF_*.npy, and {red/blue/green/all} is an optional argument
+# Where the filenames are of the form [DJDJ/VDJ]_*.npy, and {red/blue/green/all} is an optional argument
 # that determines the color choice of the plot.
 
 # red/blue/green gives shades of red/blue/green
@@ -18,17 +19,19 @@ import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import sys, statistics, math, pickle
+import sys, statistics, math, pickle, os
 import filenameManipulations as fiMa
+import separationDistribution as sepDis
 import plotManipulations as plMa
 
 figure_size = (10,7)
+defaultBinCount = 500
 
 def plotSeparationDistribution(pdf, binCenters, fig, ax, colors, pdfName, intLabel):
     dataType = fiMa.extractPrefix(pdfName)
     ax.plot(binCenters, pdf, label=pdfName, color=colors[intLabel], marker='None', linestyle='-')
-    ax.set_ylabel("PDF")
     ax.set_xlabel(u"Separation [\u03BCm]")
+    ax.set_ylabel("PDF")
     ax.set_title("Probability Density Function of " + dataType + " Separations")
     fig.tight_layout()
 
@@ -52,7 +55,7 @@ if __name__ == "__main__":
     filenames = []
     if colorFlag == -1:
         colors = ['r', 'b', 'g', 'c', 'm', 'y', 'k', 'w']
-        assert (argc - 1 <= len(colors)), "Not enough default colors. Please update plotMSD.py"        
+        assert (argc - 1 <= len(colors)), "Not enough default colors. Please update plotSeparationDistribution.py"        
         for a in range(1,argc):
             filenames.append(str(sys.argv[a]))
     elif colorFlag == 3:
@@ -66,10 +69,17 @@ if __name__ == "__main__":
     
     fig, ax = plt.subplots(figsize=figure_size)
 
+currentDir = os.getcwd()
     for index in range(len(filenames)):
-        pdf = np.load(filenames[index])
-        binCenters = np.load(fiMa.swapPrefix(filenames[index], "binCenters_"))
-        pdfName = fiMa.stripPrefix(fiMa.stripExtension(filenames[index]))
+        file1 = filenames[index]
+        pdfFile = currentDir + "/PDF_" + file1
+        binFile = currentDir + "/binCenters_" + file1
+        if os.path.isfile(pdfFile) and os.path.isfile(binFile):
+            pdf = np.load(pdfFile)
+            binCenters = np.load(binFile)
+        else:
+            pdf, binCenters = sepDis.singleTrajectoryAnalysis(file1, defaultBinCount)
+        pdfName = fiMa.stripExtension(filenames[index])
         fig, ax = plotSeparationDistribution(pdf, binCenters, fig, ax, colors, pdfName, index)
     
     ax.legend()
