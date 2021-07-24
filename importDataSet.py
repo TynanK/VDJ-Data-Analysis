0f851a2d53ab8a93594a2ab584d286a7a76eb04c
Tynan Kennedy <tynankennedy@gmail.com>
2021-07-23 19:54:05 -0700

    Updated to handle either csv or txt data

--- importDataSet.py
+++ importDataSet.py
@@ -6,17 +6,36 @@
 
 import numpy as np
 import importData
-import sys
+import sys, os
 
 def importDataSet(timestep, numCells):
     cellNumbers = list(range(1,numCells+1))
     R1_files = []
     G1_files = []
     G2_files = []
+    thisDir = os.getcwd()
     for num in cellNumbers:
-        R1_files.append("R1_" + str(num) + ".csv")
-        G1_files.append("G1_" + str(num) + ".csv")
-        G2_files.append("G2_" + str(num) + ".csv")
+        if os.path.exists(thisDir + "/R1_" + str(num) + ".csv"):
+            R1_files.append("R1_" + str(num) + ".csv")
+        elif os.path.exists(thisDir + "/R1_" + str(num) + ".txt"):
+            R1_files.append("R1_" + str(num) + ".txt")
+        else:
+            print("Error: missing R1_" + str(num) + " data")
+
+        if os.path.exists(thisDir + "/G1_" + str(num) + ".csv"):
+            G1_files.append("G1_" + str(num) + ".csv")
+        elif os.path.exists(thisDir + "/G1_" + str(num) + ".txt"):
+            G1_files.append("G1_" + str(num) + ".txt")
+        else:
+            print("Error: missing G1_" + str(num) + " data")
+
+        if os.path.exists(thisDir + "/G2_" + str(num) + ".csv"):
+            G2_files.append("G2_" + str(num) + ".csv")
+        elif os.path.exists(thisDir + "/G2_" + str(num) + ".txt"):
+            G2_files.append("G2_" + str(num) + ".txt")
+        else:
+            print("Error: missing G2_" + str(num) + " data")
+
     for a in range(numCells):
         importData.importData(R1_files[a], timestep)
         importData.importData(G1_files[a], timestep)
@@ -25,7 +44,7 @@ def importDataSet(timestep, numCells):
 
 if __name__ == "__main__":
     argc = len(sys.argv)
-    assert (argc == 3), "Imporoper inputs. Proper syntax: python3 importDataSet.py timestep numCells"
+    assert (argc == 3), "Improper inputs. Correct syntax: python3 importDataSet.py timestep numCells"
     timestep = float(sys.argv[1])
     numCells = int(sys.argv[2])
     importDataSet(timestep, numCells)
